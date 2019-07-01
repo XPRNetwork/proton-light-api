0ed20db1d4bae5a659c24a6f74e92dce68de3a66
Metallicus Dev <>
2019-07-01 03:58:49 -0600

    First commit

---
+++ index.ts
@@ -0,0 +1,7 @@
+// TypeScript Definitions
+export * from "./src/types/api";
+
+// Light API
+export * from "./src/jsonrpc";
+export * from "./src/endpoints";
+export * from "./src/rpcerror";
---
+++ src/endpoints.ts
@@ -0,0 +1,12 @@
+export const GET_ACCOUNT = "/api/account";
+export const GET_BALANCES = "/api/balances";
+export const GET_KEY_ACCOUNTS = "/api/key";
+export const GET_NETWORKS = "/api/networks";
+export const GET_SYNC_INFO = "/api/sync";
+export const GET_TOKEN_BALANCE = "/api/tokenbalance";
+export const GET_TOPHOLDERS = "/api/topholders";
+export const GET_USERCOUNT = "/api/usercount";
+export const GET_TOPRAM = "/api/topram";
+export const GET_TOPSTAKE = "/api/topstake";
+export const GET_CODEHASH = "/api/codehash";
+export const GET_TOKEN_HOLDER_COUNT = "/api/holdercount";
